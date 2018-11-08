ebe22c650417a8dffae934b07bc2b7d72ad775d8
Wisemar <44879764+Wisemar@users.noreply.github.com>
2018-11-08 21:42:46 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,27 @@
+{
+
+  "name": "Justr3kt-bot",
+
+  "version": "1.0.0",
+
+  "description": "",
+
+  "main": "index.js",
+
+  "scripts": {
+
+    "test": "echo \"Error: no test specified\" && exit 1"
+
+  },
+
+  "author": "Wisemar",
+
+  "license": "ISC",
+
+  "dependencies": {
+
+    "discord.js": "^11.3.2"
+
+  }
+
+}
