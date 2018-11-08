988276e6f5593d75d7657afd245834f4c20e19c8
Wisemar <44879764+Wisemar@users.noreply.github.com>
2018-11-08 23:43:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ bot.on('guildMemberAdd', member => {
     }).catch(console.error)
 });
 
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "site"){
+        msg.channel.send("https://alexpgm.000webhostapp.com/")
+        console.log("Une personne a demandé pour aller sur ton site.")
+    }
+
+});
 
 
 
